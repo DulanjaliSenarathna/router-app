a5dd09052d3b50f27319f5601e3ea70aa39ca61f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-15 00:05:39 +0530

    Programmatic Navigation

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    //this.props.history.push("/products");//we can click back button and can get previous page
+    this.props.history.replace('/products');//we can't click back button and can get previous page
+    
   };
 
   render() {
