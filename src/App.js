b08128e2c0de50add91d55a04dc2f6f577faa07d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-14 23:57:38 +0530

    Redirects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import NavBar from "./components/navbar";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
@@ -20,7 +20,10 @@ class App extends Component {
           <Route path={"/products"} render={(props)=> <Products sortBy="newest" {...props}/>}/>
           <Route path={"/posts/:year?/:month?"} component={Posts}/>
           <Route path={"/admin"} component={Dashboard}/>
-          <Route path={"/"} component={Home}/>
+          <Redirect from="/messages" to="posts"/>
+          <Route path={"/not-found"} component={NotFound}/>
+          <Route path={"/"} exact component={Home}/>
+          <Redirect to="/not-found"/>
           </Switch>      
         </div>
       </div>
